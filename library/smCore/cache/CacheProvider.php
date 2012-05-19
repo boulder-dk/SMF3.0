969b04d8fd9ed7d098e962632141925b7ac92895
Norv <norv@simplemachines.org>
2012-05-19 17:52:46 -0400

    Initiating cache working branch.
    Signed-off-by: Norv <norv@simplemachines.org>

---
+++ library/smCore/cache/CacheProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * smCore platform
+ *
+ * @package smCore
+ * @license MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version 1.1
+ * (the "License"); you may not use this package except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * The Original Code is smCore.
+ *
+ * The Initial Developer of the Original Code is the smCore project.
+ *
+ * Portions created by the Initial Developer are Copyright (C) 2012
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+*/
+
+interface CacheProvider
+{
+	public abstract function get($key, $ttl);
+	public abstract function put($key, $data, $ttl);
+	public abstract function invalidate();
+	public abstract function invalidateKey($key);
+}
