e7d47817a82982e537ab6e58672e9f84284ef6fb
Norv <norv@simplemachines.org>
2012-05-30 09:07:27 -0400

    Preparation for clean version.
    Signed-off-by: Norv <norv@simplemachines.org>

--- library/smCore/database/DefaultMysqlAdapter.php
+++ library/smCore/database/DefaultMysqlAdapter.php
@@ -1,14 +1,25 @@
 <?php
 
 /**
- * Database layer
+ * smCore platform
  *
  * @package database
- * @author Simple Machines http://www.simplemachines.org
- * @copyright 2012 Simple Machines and contributors
- * @license http://www.simplemachines.org/about/smf/license.php BSD
+ * @license MPL 1.1
  *
- * @version 1.0 Alpha 1
+ * The contents of this file are subject to the Mozilla Public License Version 1.1
+ * (the "License"); you may not use this package except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * The Original Code is smCore.
+ *
+ * The Initial Developer of the Original Code is Simple Machines and contributors.
+ *
+ * Portions created by the Initial Developer are Copyright (C) 2012
+ * the Initial Developer. All Rights Reserved.
+ *
+ * @version 1.0 alpha
+ *
  */
 
 namespace database;
